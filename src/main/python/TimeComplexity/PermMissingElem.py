cf221f0b259df874c13a3691e68033d03996f1f2
Kee-Yeong Tan <keeyeong.tan@gmail.com>
2015-12-27 18:52:04 +0100

    Explanation in comment for PermMissingElem Python solution

--- src/main/python/TimeComplexity/PermMissingElem.py
+++ src/main/python/TimeComplexity/PermMissingElem.py
@@ -1,2 +1,5 @@
 def solution(A):
+	# First sum is what the sum should be if all elements are there
+	# Second sum is what the sum is from the input A
+	# The difference is the missing number
 	return (sum(range(1, len(A) + 2)) - sum(A))
